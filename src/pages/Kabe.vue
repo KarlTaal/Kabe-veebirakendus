c2f28629626c447e2d28e2c52e33196ad21067ca
KarlTaal <44434743+KarlTaal@users.noreply.github.com>
2021-02-17 01:22:08 +0200

    grid näide

--- src/pages/Kabe.vue
+++ src/pages/Kabe.vue
@@ -1,40 +1,46 @@
 <template>
     <div class="konteiner">
 
-        <div style="margin-bottom: 3em;">Algus</div>
-
-        <OlenKomponent
-                v-for="(nr, index) in numbrid"
-                :key="index"
-                :number="nr"
-        />
-
-        <div style="margin-top: 3em;">Lõpp</div>
+        <div class="game-field">
+            <div
+                    v-for="nr in 13" :key="nr"
+                    class="field-item"
+            >
+                tere {{nr}}
+            </div>
+        </div>
 
     </div>
 </template>
 
 <script>
-    import OlenKomponent from "../components/OlenKomponent";
-
     export default {
         name: "Kabe",
-
-        components: {OlenKomponent},
-
-        data(){
-            return {
-                numbrid: [1, 3, 5, 6]
-            }
-        }
     }
 </script>
 
 <style scoped>
-.konteiner{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
+    .konteiner {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .game-field{
+        display: grid;
+        grid-template-columns: repeat(5, 4em);
+        gap: 2px;
+    }
+
+    .field-item{
+        background-color: #b886a6;
+        /*
+        siin saaks ruudu suuruse defineerida, sisu keskele paigutada jne.
+        Sisu saaks keskele paigutada flexboxiga siin.
+        display: flex;
+        justifiy-content:center;
+        align-items:center;
+        */
+    }
 </style>
