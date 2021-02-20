e8283967abb9361b26e42cc05999891d56f969ad
KarlTaal <44434743+KarlTaal@users.noreply.github.com>
2021-02-20 03:12:54 +0200

    build fixes

--- tests/unit/komponendid/kabeNupuTestid.spec.js
+++ tests/unit/komponendid/kabeNupuTestid.spec.js
@@ -1,23 +1,23 @@
 import {shallowMount} from "@vue/test-utils";
-import OlenKomponent from "@/components/OlenKomponent";
+import KabeNupp from "../../../src/components/KabeNupp";
 
 describe("Selectors component tests", () => {
 
   it("Test default style", () => {
-    const wrapper = shallowMount(OlenKomponent);
+    const wrapper = shallowMount(KabeNupp);
     expect(wrapper.findAll('div').at(0).classes()).toContain('must');
     expect(wrapper.findAll('div').at(0).classes()).toContain('nupp');
   });
 
   it("Test black style", async () => {
-    const wrapper = shallowMount(OlenKomponent);
+    const wrapper = shallowMount(KabeNupp);
     await wrapper.setProps({player: 'must'});
     expect(wrapper.findAll('div').at(0).classes()).toContain('must');
     expect(wrapper.findAll('div').at(0).classes()).toContain('nupp');
   });
 
   it("Test white style", async () => {
-    const wrapper = shallowMount(OlenKomponent);
+    const wrapper = shallowMount(KabeNupp);
     await wrapper.setProps({player: 'valge'});
     expect(wrapper.findAll('div').at(0).classes()).toContain('valge');
     expect(wrapper.findAll('div').at(0).classes()).toContain('nupp');
